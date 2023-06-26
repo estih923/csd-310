12bed0d4d3fdfc9883c8902935f8103c42b3b821
estih923 <128614049+estih923@users.noreply.github.com>
2023-06-26 00:01:08 -0400

    Added Module6 python file

---
+++ Module_6/mysql_test.py
@@ -0,0 +1,10 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+config = {
+    "user": "root",
+    "password": "novablue",
+    "host": "3306",
+    "database": "movies",
+    "raise_on_warnings": True
+}
